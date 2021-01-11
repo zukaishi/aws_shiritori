ff1c6ab5b9f7f15c00ae72e00e922e8c171cb29b
zukaishi <ishizuka215@gmail.com>
2021-01-11 21:48:11 +0900

    update

--- api-go/main.go
+++ api-go/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func HandleRequest(ctx context.Context, params interface{}) (interface{}, error) {
-	// TODO your processing
-	return params, nil
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	name1 := request.QueryStringParameters["name1"]
+	name2 := request.QueryStringParameters["name2"]
+	old := 25
+
+	person := PersonResponse{
+		Name1: name1,
+		Name2: name2,
+		Old:   old,
+	}
+	jsonBytes, _ := json.Marshal(person)
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(jsonBytes),
+		StatusCode: 200,
+	}, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(handler)
+}
+
+type PersonResponse struct {
+	Name1 string `json:"name1"`
+	Name2 string `json:"name2"`
+	Old   int    `json:"old"`
 }
