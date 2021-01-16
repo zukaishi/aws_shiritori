71b93492c8ede25517d10f38b291e5043252e44b
zukaishi <ishizuka215@gmail.com>
2021-01-16 16:59:54 +0900

    update

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         errors: [],
+        result: [],
         name1: null,
-        name2: null,
-        result: []
+        name2: null
     },
     methods:{
       checkForm: function (e) {
@@ -12,15 +12,16 @@ var app = new Vue({
         this.errors = [];
         let results = []
 
-        /*
         if (!this.name1) {
           this.errors.push('Name1 required.');
         }
         if (!this.name2) {
           this.errors.push('Name2 required.');
         }
-*/
-        axios.get('https://jiehmlfyck.execute-api.ap-northeast-1.amazonaws.com/default/api-test')
+
+        axios.get('https://jiehmlfyck.execute-api.ap-northeast-1.amazonaws.com/default/api-test' 
+                    + "?name1=" + this.name1 
+                    + "&name2=" + this.name2 )
           .then((response) => {
             str = response.data.body.split(",");
             str.forEach(function(str){
