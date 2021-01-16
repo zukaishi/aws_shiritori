06092b407bff793c22d1e99ccbdcbff83fd69812
zukaishi <ishizuka215@gmail.com>
2021-01-16 16:55:18 +0900

    update

--- static/js/index.js
+++ static/js/index.js
@@ -4,13 +4,15 @@ var app = new Vue({
         errors: [],
         name1: null,
         name2: null,
-        result: null
+        result: []
     },
     methods:{
       checkForm: function (e) {
         e.preventDefault();
         this.errors = [];
-/*
+        let results = []
+
+        /*
         if (!this.name1) {
           this.errors.push('Name1 required.');
         }
@@ -18,14 +20,16 @@ var app = new Vue({
           this.errors.push('Name2 required.');
         }
 */
-
-
+        axios.get('https://jiehmlfyck.execute-api.ap-northeast-1.amazonaws.com/default/api-test')
+          .then((response) => {
+            str = response.data.body.split(",");
+            str.forEach(function(str){
+              results.push(str);
+            })
+          })
+          this.result = results
         }
     },
     mounted () {
-      axios.get('https://jiehmlfyck.execute-api.ap-northeast-1.amazonaws.com/default/api-test')
-        .then((response) => {
-          this.result = response.data.body;
-        })
     }
 });
