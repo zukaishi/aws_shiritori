bd8e729844dede29824ead1012e40839287f1acd
zukaishi <ishizuka215@gmail.com>
2021-01-28 06:55:07 +0900

    update

--- static/js/index.js
+++ static/js/index.js
@@ -15,13 +15,15 @@ var app = new Vue({
         if (!this.name1) {
           this.errors.push('Name1 required.');
         }
+        let url =""
         if (!this.name2) {
+          url = "https://2wb8kl0nf6.execute-api.ap-northeast-1.amazonaws.com/default/comprised" + "?name1=" + this.name1 
           this.errors.push('Name2 required.');
+        } else {
+          url = "https://jiehmlfyck.execute-api.ap-northeast-1.amazonaws.com/default/api-test" + "?name1=" + this.name1 + "&name2=" + this.name2 
         }
 
-        axios.get('https://jiehmlfyck.execute-api.ap-northeast-1.amazonaws.com/default/api-test' 
-                    + "?name1=" + this.name1 
-                    + "&name2=" + this.name2 )
+        axios.get(url)
           .then((response) => {
             str = response.data.body.split(",");
             str.forEach(function(str){
