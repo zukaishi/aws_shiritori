e2e2c7b61e8fe7a77a05b9c5ba590313dbc6565f
zukaishi <ishizuka215@gmail.com>
2021-02-02 06:37:09 +0900

    update

--- static/js/index.js
+++ static/js/index.js
@@ -11,6 +11,8 @@ var app = new Vue({
       request: function (e) {
         e.preventDefault();
         this.errors = [];
+        let name1 = this.name1
+        let name2 = this.name2
 
         if (!this.name1) {
           this.errors.push('Name1 required.');
@@ -23,20 +25,35 @@ var app = new Vue({
 
         let url =""
         if (!this.name2) {
-          url = "https://2wb8kl0nf6.execute-api.ap-northeast-1.amazonaws.com/default/comprised" + "?name1=" + this.name1
+          url = "https://2wb8kl0nf6.execute-api.ap-northeast-1.amazonaws.com/default/comprised" + "?name1=" + name1
         } else {
-          url = "https://jiehmlfyck.execute-api.ap-northeast-1.amazonaws.com/default/api-test" + "?name1=" + this.name1 + "&name2=" + this.name2 
+          url = "https://jiehmlfyck.execute-api.ap-northeast-1.amazonaws.com/default/api-test" + "?name1=" + name1+ "&name2=" + name2
         }
 
         let mode = this.mode 
         let results = []
         axios.get(url)
           .then((response) => {
-            let body = this.kanaToHira(response.data.body)
-            str = body.split(",");
-            str.forEach(function(str){
-              results.push(str);
-            })
+            let body = response.data.body.split(",");
+            // https://ja.stackoverflow.com/questions/72150/vue%E3%81%AEforeach%E3%81%AE%E4%B8%AD%E3%81%A7%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89%E3%81%8C%E5%91%BC%E3%81%B9%E3%81%AA%E3%81%84
+            body.forEach((value) => {
+              if( value ) {
+
+                let [no,name] = value.split(':');
+                if ( mode == 2 ) {
+                  if( name1 !== name.slice(0,name1.length) ) {
+                    name = ""
+                  }
+                } else if ( mode == 3 ) {
+                  if( name1 !== name.slice(name1.length*-1) ) {
+                    name = ""
+                  }
+                }
+                if (name ) {
+                  results.push(  name + "(" + this.kanaToHira(name) + ")");
+                }
+              }
+            });
           })
         this.result = results
       },
